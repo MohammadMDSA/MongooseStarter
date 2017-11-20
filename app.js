c26388c0ad35137f5de1bce217a25d1d37c7e12b
Mohammad MDSA <samimd.77@gmail.com>
2017-11-20 13:19:54 +0330

    Setup bot framework

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 require('dotenv-extended').load();
 const builder = require('botbuilder');
+const { Prompts, Message, UniversalBot, ChatConnector } = builder;
+const restify = require('restify');
+
+const server = restify.createServer();
+server.listen(process.env.PORT, () => {
+	console.log(`${server.name} listening to ${server.url}`);
+});
+
+let connector = new ChatConnector({
+	appId: process.env.MICROSOFT_APP_ID,
+	appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+server.post('api/messages', connector.listen());
+
+let bot = new UniversalBot(connector, (session) => {
+	// Bot code goes here...
+});
